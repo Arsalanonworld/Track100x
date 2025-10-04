6c7d4437a51d50bd702684431d13e1f2bff20832
Arsalan <arsalanonworld@gmail.com>
2025-10-04 10:24:16 +0000

    i still cannot see icon images

--- src/components/crypto-icon.tsx
+++ src/components/crypto-icon.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { cn } from '@/lib/utils';
-import { Badge } from './ui/badge';
 
 type CryptoIconProps = {
   token: 'BTC' | 'ETH' | 'SOL' | 'USDT' | 'USDC' | 'WIF' | string;
