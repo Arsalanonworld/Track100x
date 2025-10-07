24cb17fb519acdaf5d2558cfccbbe1e897299908
Arsalan <arsalanonworld@gmail.com>
2025-10-07 16:45:59 +0000

    try again,

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,8 +11,6 @@ import { useUser } from "@/firebase";
 export const LogoIcon = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -21,13 +19,8 @@ export const LogoIcon = () => (
       strokeLinejoin="round"
       className="h-7 w-7 text-primary"
     >
-      <path d="M2 14c.6.9 1.3 1.8 2.2 2.5" />
-      <path d="M12 11h8" />
-      <path d="M12 15h6" />
-      <path d="M12 19h4" />
-      <path d="M4.2 16.5c-1.3-1.6-2-3.5-2.2-5.5" />
-      <path d="M10 9.5c-1.3-1.5-1.3-4.5.2-6" />
-      <path d="M14 9.5c1.3-1.5 1.3-4.5-.2-6" />
+        <path d="M4 4l4 12 4-8 4 8 4-12"/>
+        <path d="M4 16h16"/>
     </svg>
 );
 
