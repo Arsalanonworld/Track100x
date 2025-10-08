c1a41799abea11164fb6ca269c421400ef6b59b7
Arsalan <arsalanonworld@gmail.com>
2025-10-08 17:37:18 +0000

    improve the design

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -19,10 +19,10 @@ export const LogoIcon = () => (
       strokeLinejoin="round"
       className="h-7 w-7 text-primary"
     >
-      <path d="M7 12c0 3.31 2.69 6 6 6s6-2.69 6-6-2.69-6-6-6" />
-      <path d="M12 18c-3.31 0-6-2.69-6-6" />
-      <path d="M12 6a4 4 0 0 1 4 4" />
-      <path d="M16 12a2 2 0 0 1-2 2" />
+      <path d="M6 12c0-3.31 2.69-6 6-6s6 2.69 6 6c0 1.4-1.87 4-6 4s-6-2.6-6-4Z" />
+      <path d="m14.5 16.5-2.5 3-2.5-3" />
+      <path d="M12 12V6" />
+      <path d="M18 12a3 3 0 0 0-3-3" />
     </svg>
 );
 
