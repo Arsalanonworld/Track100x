b03e9e661844f91f5d5894d8a865aca0de8d2484
Arsalan <arsalanonworld@gmail.com>
2025-10-08 17:59:46 +0000

    anything absctract?

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -19,10 +19,9 @@ export const LogoIcon = () => (
       strokeLinejoin="round"
       className="h-7 w-7 text-primary"
     >
-      <path d="M12 8L12 16" />
-      <path d="M9 13L12 16L15 13" />
-      <path d="M17.65 6.35a9 9 0 1 1-11.3 0" />
-      <path d="M15.53 8.47a5 5 0 1 1-7.06 0" />
+        <path d="M12 2L2 7l10 5 10-5-10-5z" />
+        <path d="M2 17l10 5 10-5" />
+        <path d="M2 12l10 5 10-5" />
     </svg>
 );
 
