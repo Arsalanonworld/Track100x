342a691e0701d3a98711c0ebc15ce6dabad094f3
Arsalan <arsalanonworld@gmail.com>
2025-10-08 18:02:45 +0000

    i cannot see the spark

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -19,9 +19,8 @@ export const LogoIcon = () => (
       strokeLinejoin="round"
       className="h-7 w-7 text-primary"
     >
-        <path d="M12 2L2 7l10 5 10-5-10-5z" />
-        <path d="M2 17l10 5 10-5" />
-        <path d="M2 12l10 5 10-5" />
+        <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2z"/>
+        <path d="m13.29 7.71-4.58 8.58" />
     </svg>
 );
 
