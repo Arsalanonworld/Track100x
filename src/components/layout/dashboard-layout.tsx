c93243d32609f4af9cda40a1135da7037124f6cf
Arsalan <arsalanonworld@gmail.com>
2025-10-27 04:06:01 +0000

    yes

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -5,13 +5,15 @@ import React from 'react';
 import { Footer } from './footer';
 import Header from './header';
 import { cn } from '@/lib/utils';
+import { TickerBar } from './ticker-bar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
+      <TickerBar />
       <div className="flex flex-col w-full">
-        <main className="flex-1 p-4 lg:p-6 pt-20 lg:pt-24 min-h-screen container">
+        <main className="flex-1 p-4 lg:p-6 pt-24 lg:pt-28 min-h-screen container">
             {children}
         </main>
         <Footer />
